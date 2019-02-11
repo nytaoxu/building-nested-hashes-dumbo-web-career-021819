e9d5080604c02690a8877feef7a247b2d08cfa53
Tao Xu <nytaoxu@gmail.com>
2019-02-11 22:06:53 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,7 +8,8 @@ def fourth_challenge
         #your hashes here!
         {
           :name => "Benvolio",
-          :age => "17"
+          :age => "17",
+          :attitude => "worried"
         }
       ]
    }, 
