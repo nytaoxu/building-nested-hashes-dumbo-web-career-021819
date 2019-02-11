3850fb3c76b2a506150aef25bc9d26913efd49f7
Tao Xu <nytaoxu@gmail.com>
2019-02-11 22:07:37 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -14,7 +14,7 @@ def fourth_challenge
         {
           :name => "Mercutio",
           :age => "18",
-          
+          :attitude => "hot-headed"
         }
       ]
    }, 
@@ -24,6 +24,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        
       ]
    }
 }
