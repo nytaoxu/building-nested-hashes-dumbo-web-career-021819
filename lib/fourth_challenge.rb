d5500f13c3b63da18397ceb1e72a947c62a45d25
Tao Xu <nytaoxu@gmail.com>
2019-02-11 22:06:41 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,7 +7,8 @@ def fourth_challenge
       :hero_friends => [
         #your hashes here!
         {
-          :name => "Benvolio"
+          :name => "Benvolio",
+          :age => "17"
         }
       ]
    }, 
