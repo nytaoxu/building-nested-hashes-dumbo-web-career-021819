2c33070aebe2f5c4a7c4fb6c78175e03807709ae
Tao Xu <nytaoxu@gmail.com>
2019-02-11 22:03:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,8 @@ def third_challenge
       :hero => {
         #your key/value pairs here
           :name => "Romeo",
-          
+          :age => "15",
+          :status => 
         },
       :hero_friends => []
    }, 
